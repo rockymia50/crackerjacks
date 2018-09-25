bb22c10e06742f76e563a91f7c09582e3db6911e
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-09-25 11:22:24 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const Lorem = require('react-lorem-component');
+ 
+ const App = () => (
+      <div>
+        <h1>Lorem Ipsum</h1>
+        <Lorem />
       </div>
     );
-  }
-}
+ 
 
+    
 export default App;
